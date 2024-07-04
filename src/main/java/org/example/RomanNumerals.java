a64692878a1e7db1aa6b533d1fcc53f0b4898c76
Jenny Messer <jennylmesser@gmail.com>
2024-07-04 14:33:04 +0100

    feat: switch statement for example cases, test passes

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -1,7 +1,35 @@
 package org.example;
 
 public class RomanNumerals {
+
+        /*
+   1 ➔ I
+   2 ➔ II
+   3 ➔ III
+   4 ➔ IV
+   5 ➔ V
+   9 ➔ IX
+  21 ➔ XXI
+  50 ➔ L
+ 100 ➔ C
+ 500 ➔ D
+1000 ➔ M
+     */
+
     public static String convert(int integer) {
-        return "";
+        return switch (integer) {
+            case 1 -> "I";
+            case 2 -> "II";
+            case 3 -> "III";
+            case 4 -> "IV";
+            case 5 -> "V";
+            case 9 -> "IX";
+            case 21 -> "XXI";
+            case 50 -> "L";
+            case 100 -> "C";
+            case 500 -> "D";
+            case 1000 -> "M";
+            default -> "";
+        };
     }
 }
